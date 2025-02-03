1e711e25fbb8ed5a9357efa8c9f71c36aedfe680
Raven Hall <rzh0085@auburn.edu>
2025-02-02 22:29:18 -0500

    Initial Commit

---
+++ assignment3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment3
+//
+//  Created by Raven Pearson on 2/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ assignment3Tests/assignment3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  assignment3Tests.swift
+//  assignment3Tests
+//
+//  Created by Raven Pearson on 2/2/25.
+//
+
+import Testing
+@testable import assignment3
+
+struct assignment3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
